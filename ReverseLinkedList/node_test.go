994b1e279e90df2b33c45b26302d321fc21d7cbf
xxncxbc <674holland@gmail.com>
2025-03-31 20:55:57 +0300

    reversing linked list

--- ReverseLinkedList/node_test.go
+++ ReverseLinkedList/node_test.go
@@ -14,7 +14,7 @@ func TestReverseLinkedList(t *testing.T) {
 		{[]int{1, 0, 1, 0, 1}, "1->0->1->0->1->nil"},
 		{[]int{}, "nil"},
 		{[]int{1, 2}, "2->1->nil"},
-		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "10->9->8->7->6->5->4->3->2->1->nil"},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "10->9->8->7->6->5->4->3-2->1->nil"},
 	}
 
 	for i, test := range tests {
@@ -27,6 +27,5 @@ func TestReverseLinkedList(t *testing.T) {
 				t.Error("wrong results!")
 			}
 		})
-
 	}
 }
